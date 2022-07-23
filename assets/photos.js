1b77f2d07041d083cbcbeb5d37abec3b5be88043
zzpapy <gregory.pace@hotmail.fr>
2022-07-23 14:15:43 +0200

    first commit

---
+++ assets/photos.js
@@ -0,0 +1,86 @@
+// $("#click").on('click', (e) => {
+//     e.preventDefault()
+//     let url = $(e.target).data('url')
+//     $.post(url, $( "#tchat").serialize(),function (json) {
+//         console.log(Object.values(json.message))
+//         $(".listMess").html("")
+//         Object.values(json.message).forEach(element => {
+//             $(".listMess").append( "<div><p>"+element.user+"</p><p>"+element.text+"</p></div>")
+            
+//         });
+//         console.log(json.message)
+       
+       
+//     },"json" ).done( function (result) { 
+//         console.log(result.message)
+//         result.message.forEach(element => {
+//             console.log(element)
+            
+//         });
+//     })
+// })
+
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import React , { useEffect, useState }from 'react';
+
+
+
+
+class App extends React.Component {
+   
+    constructor(props) {
+        super(props);   
+       
+        this.state = {
+            photos : {},
+           
+            };
+           
+    }
+  
+    
+    componentDidMount() {
+        const url = "photos"
+        fetch(url)
+        .then(async res => {
+        return res.json()
+        })
+        .then(async res => {
+            console.log(res.photos,this.state)
+            let listPhotos = []
+            res.photos.map(el => {
+                console.log("/photos/"+el)
+                let test = el.replace("jpg","jpg")
+                console.log(el.indexOf("JPG"), el.replace("jpg","jpg"),typeof el,test)
+                listPhotos.push(require("./photos/"+test))
+            })
+            this.setState({photos : listPhotos}) 
+            
+            })
+            .catch(er => console.error(er))     
+    }
+   
+        render() {
+       console.log(this.state.photos)
+    return (
+            <div className="container">
+                {this.state.photos.length != undefined ?  this.state.photos.map(el =>{
+                    return (
+                        <div className="photos">
+                            <img src={el} ></img>
+                        </div>
+                    )
+                }) :null}
+                test
+            </div>     
+        ) 
+    }
+    
+}
+if(document.getElementById('photos') != null){
+    const container = document.getElementById('photos')
+    const root = createRoot(container);
+    root.render(<App />);
+
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Finder\Finder;
+use App\Service\ContainerParametersHelper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ContainerParametersHelper $pathHelpers): Response
+    {
+      
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/photos', name: 'app_photos')]
+    public function photos(Request $request, ContainerParametersHelper $pathHelpers): Response
+    {
+        $response = new Response();
+
+        $finder = new Finder();
+        // find all files in the current directory
+        $finder->files()->in($pathHelpers->getApplicationRootDir().'\assets\photos');
+        
+        
+        if ($finder->hasResults()) {
+            // ...
+        }
+        $list = [];
+        foreach ($finder as $file) {
+            $absoluteFilePath = $file->getRealPath();
+            $fileNameWithExtension = $file->getRelativePathname();
+            array_push($list,$fileNameWithExtension);
+            
+            // ...
+        }
+        dump($list);
+        $response->setContent(json_encode([
+            "photos" => $list
+        ]));
+    
+        return $response;
+    }
+}
