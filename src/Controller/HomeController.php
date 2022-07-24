16b7c36f78f649e29652b6730433d8382b943456
zzpapy <gregory.pace@hotmail.fr>
2022-07-24 17:19:36 +0200

    reset

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,9 +14,27 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(ContainerParametersHelper $pathHelpers): Response
     {
+        $finder = new Finder();
+        // find all files in the current directory
+        $finder->files()->in($pathHelpers->getApplicationRootDir().'/public/photos');
+        
+        
+        if ($finder->hasResults()) {
+            // ...
+        }
+        $list = [];
+        foreach ($finder as $file) {
+            $absoluteFilePath = $file->getRealPath();
+            $fileNameWithExtension = $file->getRelativePathname();
+            array_push($list,$fileNameWithExtension);
+            
+            // ...
+        }
+        dump($absoluteFilePath);
+
       
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'list' => $list,
         ]);
     }
     #[Route('/photos', name: 'app_photos')]
